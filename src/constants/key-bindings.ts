e7b9e21dcbcbba38e16ec112759c613942dcff2e
Saiem Saeed <saiem.saeed7@gmail.com>
2025-03-09 01:25:12 +0100

    Created Cursor Handlers in Seperate Helper Files

--- src/constants/key-bindings.ts
+++ src/constants/key-bindings.ts
@@ -6,5 +6,9 @@ export const KEY_BINDINGS: IKEYBINDINGS = {
   MOVE_LEFT: "ArrowLeft",
   MOVE_RIGHT: "ArrowRight",
   NEW_LINE: "Enter",
-  DELETE: "Backspace",
+  DELETE_LINE: "Backspace",
+  MOVE_NEXT_WORD: "Alt+ArrowRight",
+  MOVE_PREV_WORD: "Alt+ArrowLeft",
+  MOVE_START_LINE: "Home",
+  MOVE_END_LINE: "End",
 };
